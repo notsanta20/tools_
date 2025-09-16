e4d2ed3d10a577a69242ef949dcfc1bc8ebd5585
santhosh <santhosh.003z@gmail.com>
2025-09-16 19:02:23 +0530

    feat: add error message and moved value to state

--- src/components/ui/forms/TextArea.tsx
+++ src/components/ui/forms/TextArea.tsx
@@ -1,19 +1,37 @@
+import type { FieldError } from "react-hook-form";
+
 function TextArea({
   label,
   placeholder,
+  value,
+  setValue,
+  register,
+  error,
 }: {
   label: string;
   placeholder: string;
+  value: string;
+  setValue: Function;
+  register: Function;
+  error: FieldError | undefined;
 }) {
   return (
     <div className="text-white flex flex-col gap-2 text-xs">
-      <label htmlFor={label}>{label}</label>
+      <div className="flex gap-2">
+        <label htmlFor={label}>{label}</label>
+        <div className="text-red-700">
+          {typeof error == "undefined" ? "" : error.message}
+        </div>
+      </div>
       <textarea
-        name={label}
-        id={label}
+        {...register(label)}
         placeholder={placeholder}
         className="border-1 border-(--gray-dark) p-1.5 outline-none"
         rows={3}
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
       />
     </div>
   );
