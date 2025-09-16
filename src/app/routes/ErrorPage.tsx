d9a8eb3c61ff6910b824b19ce070f8c15f884a99
santhosh <santhosh.003z@gmail.com>
2025-09-16 18:53:34 +0530

    feat: added error page

---
+++ src/app/routes/ErrorPage.tsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+
+function ErrorPage() {
+  return (
+    <main className="flex flex-col gap-2 h-full">
+      <Header />
+      <section className="p-2 flex-auto flex flex-col gap-2 justify-center items-center">
+        <h2 className="text-4xl">error 404</h2>
+        <p className="text-sm">page not found</p>
+      </section>
+    </main>
+  );
+}
+
+export default ErrorPage;
