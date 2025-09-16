8c192dc2cce3759f17a24ab4be4da1c7500d182d
santhosh <santhosh.003z@gmail.com>
2025-09-16 18:54:23 +0530

    feat: added copy to clipboard function

---
+++ src/utils/copyToClipboard.ts
@@ -0,0 +1,9 @@
+async function copyToClipboard(text: string) {
+  try {
+    await navigator.clipboard.writeText(text);
+  } catch (err) {
+    console.error("Failed to copy text: ", err);
+  }
+}
+
+export default copyToClipboard;
