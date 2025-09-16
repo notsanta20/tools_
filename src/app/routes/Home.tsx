186d35db63c71723817ff8b259b8b90776c93381
santhosh <santhosh.003z@gmail.com>
2025-09-16 23:37:10 +0530

    fix: removed unused style names

--- src/app/routes/Home.tsx
+++ src/app/routes/Home.tsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 function Home() {
   return (
-    <main className="bg-black text-white font-(family-name:--font-mono) flex flex-col gap-2 py-3 px-[20%] h-screen">
+    <main className="flex flex-col gap-2 h-full">
       <Header />
       <section className="p-2 flex-auto flex justify-center items-center">
         home of tools
