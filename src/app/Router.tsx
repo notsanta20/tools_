6116030ded157a86483166a1dc2eb0b6aeeec0c6
santhosh <santhosh.003z@gmail.com>
2025-09-16 23:36:38 +0530

    fix: added container for padding and added error page route

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router";
 import Home from "./routes/Home";
 import HashGenerator from "./routes/app/HashGenerator";
+import ErrorPage from "./routes/ErrorPage";
 
 function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/hash-generator" element={<HashGenerator />} />
-    </Routes>
+    <div className="px-[20%] py-3 font-(family-name:--font-mono) text-white h-screen">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hash-generator" element={<HashGenerator />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
   );
 }
 
