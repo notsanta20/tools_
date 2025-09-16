d72bd8738a290a4bfd7c886d231f201414f6cc3b
santhosh <santhosh.003z@gmail.com>
2025-09-16 18:57:53 +0530

    feat: added clear button component

---
+++ src/components/ui/forms/ClearButton.tsx
@@ -0,0 +1,23 @@
+import type { MouseEventHandler } from "react";
+
+function ClearButton({
+  label,
+  func,
+}: {
+  label: string;
+  func: MouseEventHandler<HTMLButtonElement>;
+}) {
+  return (
+    <input
+      type="button"
+      id={label}
+      value={label}
+      onClick={() => {
+        func();
+      }}
+      className="border-1 border-(--gray-dark) py-2 px-4 bg-black hover:bg-(--gray-dark) hover:cursor-pointer text-sm"
+    />
+  );
+}
+
+export default ClearButton;
