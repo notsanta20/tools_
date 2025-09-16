24ffbd7dd619bfc333bdc78a8b42b6f928547b57
santhosh <santhosh.003z@gmail.com>
2025-09-16 23:35:57 +0530

    feat: moved hash generator to separate component and added verify component

--- src/app/routes/app/HashGenerator.tsx
+++ src/app/routes/app/HashGenerator.tsx
@@ -1,23 +1,16 @@
 import Header from "../Header";
-import TextArea from "../../../components/ui/forms/TextArea";
-import CopyInput from "../../../components/ui/forms/CopyInput";
+import GenerateHash from "./GenerateHash";
+import VerifyHash from "./VerifyHash";
 
 function HashGenerator() {
   return (
     <>
-      <main className="bg-black text-white font-(family-name:--font-mono) flex flex-col gap-2 py-3 px-[20%]">
+      <main className="font-(family-name:--font-mono) flex flex-col gap-2">
         <Header />
         <section className="p-2 flex flex-col gap-3">
-          <h2>hash generator</h2>
-          <form action="" className="flex flex-col gap-2">
-            <TextArea label="Text" placeholder="input string to hash" />
-            <CopyInput label="salt" placeholder="random salt" checkBox={true} />
-            <CopyInput
-              label="hashcode"
-              placeholder="hashcode"
-              checkBox={false}
-            />
-          </form>
+          <h2 className="font-semibold">hash generator</h2>
+          <GenerateHash />
+          <VerifyHash />
         </section>
       </main>
     </>
