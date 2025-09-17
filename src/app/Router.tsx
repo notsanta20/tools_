2b6f38952996a66ddda46837eababbb3b3ff572d
santhosh <santhosh.003z@gmail.com>
2025-09-17 11:48:09 +0530

    feat: added header, footer and under construction page

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -2,15 +2,22 @@ import { Routes, Route } from "react-router";
 import Home from "./routes/Home";
 import HashGenerator from "./routes/app/HashGenerator";
 import ErrorPage from "./routes/ErrorPage";
+import Header from "./routes/Header";
+import Footer from "./routes/Footer";
+import UnderConstruction from "./routes/UnderConstruction";
 
 function Router() {
   return (
-    <div className="px-[20%] py-3 font-(family-name:--font-mono) text-white h-screen">
+    <div className="px-[20%] py-3 font-(family-name:--font-mono) text-white h-screen flex flex-col">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/all-tools" element={<UnderConstruction />} />
         <Route path="/hash-generator" element={<HashGenerator />} />
+        <Route path="/password-generator" element={<UnderConstruction />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
