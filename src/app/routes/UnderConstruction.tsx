b3ca7d62927d5d98444893d788a6a11ec298c8c0
santhosh <santhosh.003z@gmail.com>
2025-09-17 11:46:33 +0530

    feat: added under construnction component

---
+++ src/app/routes/UnderConstruction.tsx
@@ -0,0 +1,12 @@
+function UnderConstruction() {
+  return (
+    <main className="flex flex-col gap-2 h-full">
+      <section className="p-2 flex-auto flex flex-col gap-2 justify-center items-center">
+        <h2 className="text-4xl">page Under construction</h2>
+        <p className="text-sm">will be back soon</p>
+      </section>
+    </main>
+  );
+}
+
+export default UnderConstruction;
