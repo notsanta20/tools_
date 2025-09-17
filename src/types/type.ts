f37d97277c53671ecbd210e8a09be1db5070c74f
santhosh <santhosh.003z@gmail.com>
2025-09-17 15:48:29 +0530

    feat: added passwordsize and totalsize types

--- src/types/type.ts
+++ src/types/type.ts
@@ -6,3 +6,15 @@ export interface verifyHashType {
   text: string;
   hash: string;
 }
+
+export interface passwordSize {
+  total: number;
+  num: number;
+  symbol: number;
+}
+
+export interface totalSize {
+  totalLetters: number;
+  totalNumbers: number;
+  totalSymbols: number;
+}
