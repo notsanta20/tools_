a40198edceeea9f889b9b6f44e75ee16846c9b8f
santhosh <santhosh.003z@gmail.com>
2025-09-17 15:47:37 +0530

    feat: added link to portfolio page

--- src/app/routes/Footer.tsx
+++ src/app/routes/Footer.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 function Footer() {
   const date: Date = new Date();
   const year: number = date.getFullYear();
@@ -5,7 +7,10 @@ function Footer() {
   return (
     <footer className="border-t-2 border-(--gray-dark) p-2 flex">
       <h2 className="text-xs font-medium flex-auto text-(--gray-light)">
-        Copyright (c) {year} made by santhosh.dev
+        Copyright (c) {year} made by{" "}
+        <Link to={"https://santhosh.dev"} target="blank">
+          santhosh.dev
+        </Link>
       </h2>
     </footer>
   );
